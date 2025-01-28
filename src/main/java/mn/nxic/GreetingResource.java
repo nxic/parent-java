1a0cddbb6bef4943f7f2adba8daacdbe5bf33874
Yondonjamts Ganzorig <yondonjamts@outlook.com>
2025-01-28 19:23:09 +0800

    updated service

--- src/main/java/mn/nxic/GreetingResource.java
+++ src/main/java/mn/nxic/GreetingResource.java
@@ -1,16 +1,26 @@
 package mn.nxic;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import mn.nxic.service.WorldTwoService;
+import mn.nxic.service.WorldOneService;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    WorldTwoService twoService;
+    WorldOneService oneService;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        twoService.sayHello();
+        oneService.sayHello();
+
         return "Hello from Quarkus REST";
     }
 }
